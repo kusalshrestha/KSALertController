44f99be6c60050c1b089a03af614055e3ce33471
kusal <shrstha.kusal@hotmail.com>
2016-05-05 11:14:32 +0545

    Made self framework

---
+++ KSDateAlertController.framework/Headers/KSDateAlertController.h
@@ -0,0 +1,19 @@
+//
+//  KSDateAlertController.h
+//  KSDateAlertController
+//
+//  Created by Kusal Shrestha on 5/5/16.
+//  Copyright © 2016 Kusal Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSDateAlertController.
+FOUNDATION_EXPORT double KSDateAlertControllerVersionNumber;
+
+//! Project version string for KSDateAlertController.
+FOUNDATION_EXPORT const unsigned char KSDateAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSDateAlertController/PublicHeader.h>
+
+
